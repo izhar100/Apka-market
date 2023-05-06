798a7d264019f0ddddab97c48c7915ff278eff44
izhar100 <ezharashraf523@gmail.com>
2023-05-06 22:31:58 +0530

    day-4-work home_page done filter sort search done

--- fresh-food/src/App.js
+++ fresh-food/src/App.js
@@ -1,12 +1,23 @@
 
+import { useSelector } from 'react-redux';
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import NavbarSmallScreen from './components/NavbarSmallScreen';
 import MainRoutes from './pages/MainRoutes';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/LoginSignUpPage/Login';
+import SplaceScreen from './pages/splaceScreen/SplaceScreen';
 
 function App() {
+  // const isAuth=useSelector((store)=>store.AuthReducer.isAuth)
+  // if(isAuth==false){
+  //   return (
+  //     <div className="App" style={{backgroundColor:"GhostWhite"}}>
+  //       <SplaceScreen/>
+  //     </div>
+  //   )
+  // }
   return (
     <div className="App" style={{backgroundColor:"GhostWhite"}}>
       <Navbar/>
--- fresh-food/src/pages/Home/DealNow.jsx
+++ fresh-food/src/pages/Home/DealNow.jsx
@@ -5,7 +5,7 @@ const DealNow = () => {
     return (
         <>
             <Box position={"relative"} w={"90%"} m={"auto"} borderRadius={"20px"}>
-                <Box p={"20px"} bgColor={"#2d2d2da1"} borderRadius={"20px"} zIndex={1} position={"absolute"} top={"30%"} right={"5%"} left={"5%"} textAlign={"center"}>
+                <Box p={"20px"} bgColor={"#2d2d2da1"} borderRadius={"20px"} position={"absolute"} top={"30%"} right={"5%"} left={"5%"} textAlign={"center"}>
                     <Text color={"#ffffff"} fontSize={{lg:"35px",xl:"35px",md:"35px",sm:"15px",base:"15px"}} as={"b"}>Shop now and Get 30% off on Fruits and Vegetables</Text>
                     <br />
                     <Text fontSize={{lg:"35px",xl:"35px",md:"35px",sm:"15px",base:"15px"}} color={"#00ffe5"} as={"b"}>Delivery time between 1-2 hours</Text>
